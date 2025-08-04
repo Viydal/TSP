0b1a518acef980ce2a28e46deed28c8858db0261
BryceYoung <bryce.young18@icloud.com>
2025-08-04 18:47:40 +0930

    Added Random and Math Imports Added fn randomPath to TSP Added fn pathCost to TSP Added fn distance to Point Added var path to TSP Added empty fn run - will be used in child classes to implement searching functions.

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 def dprint(val):
     print(val)
 
@@ -7,6 +10,8 @@ class Point:
     def __init__(self,x: int, y: int):
         self.x = x
         self.y = y
+    def distance(self,other):
+        return math.sqrt(math.pow(self.x-other.x,2)+math.pow(self.y-other.y,2))
     def print(self):
         print(self.x,self.y)
 
@@ -21,8 +26,10 @@ class City:
 
 class TSP:
     Cities: ['City']
+    path: ['City']
     def __init__(self,filename: str):
         self.Cities = []
+        self.path = []
         self.open(filename)
         pass
     def open(self,filename):
@@ -40,11 +47,31 @@ class TSP:
         while line[0:3] != "EOF":
             #dprint(line)
             ex, x, y = line.split()
+            ex = int(ex)
+            y = int(y)
+            x = int(x)
             self.Cities.append(City(Point(x,y)))
             line = f.readline().strip()
         #print(f.readline().strip())
+    def pathCost(self,path = None):
+        if path is None:
+            path = self.path
+        cost =  0
+        for i in range(len(path)-1):
+                cost += path[i].point.distance(path[i+1].point)
+        cost += path[-1].point.distance(path[0].point)
+        return cost
+    def randomPath(self):
+        cities = self.Cities
+        path = []
+        for i in range(len(cities)):
+            path.append(cities.pop(random.randint(0, len(cities)-1)))
+        return path
+    def run(self):
+        pass
 
-#travel = TSP("st70.tsp")
+
+#travl = TSP("st70.tsp")
 travl = TSP("eil101.tsp")
-for i in travl.Cities:
-    i.print()
+travl.path = travl.randomPath()
+print(travl.pathCost())
