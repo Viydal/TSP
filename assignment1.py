d29b444e8f3c68515363064d2dd95834c56a198c
BryceYoung <bryce.young18@icloud.com>
2025-08-04 14:48:50 +0930

    Commented out excess print statements

--- assignment1.py
+++ assignment1.py
@@ -7,12 +7,16 @@ class Point:
     def __init__(self,x: int, y: int):
         self.x = x
         self.y = y
+    def print(self):
+        print(self.x,self.y)
 
 
 class City:
     point: Point
     def __init__(self,point: Point):
         self.point = point
+    def print(self):
+        self.point.print()
 
 
 class TSP:
@@ -31,14 +35,16 @@ class TSP:
         #print(count)
         while line[0:18] != "NODE_COORD_SECTION":
             line = f.readline()
-        print(line.strip())
+        #print(line.strip())
         line = f.readline().strip()
         while line[0:3] != "EOF":
-            dprint(line)
+            #dprint(line)
             ex, x, y = line.split()
             self.Cities.append(City(Point(x,y)))
             line = f.readline().strip()
-        print(f.readline().strip())
+        #print(f.readline().strip())
 
 #travel = TSP("st70.tsp")
 travl = TSP("eil101.tsp")
+for i in travl.Cities:
+    i.print()
