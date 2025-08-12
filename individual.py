7f086637764eb280e075d47eed5f19b47cbbabd9
viydal <viydal04@gmail.com>
2025-08-12 15:47:27 +0930

    perform mutation works

--- individual.py
+++ individual.py
@@ -1,4 +1,5 @@
 import tsp
+import copy
 import random
 
 
@@ -25,35 +26,49 @@ class Individual:
         temp = self.path[i]
         self.path[i] = self.path[j]
         self.path[j] = temp
+        
+        self.evaluate()
+        
+        print(f"Swapped cities {i}, and {j}\n")
 
-    def inversion(self):
-        i = random.randint(0, len(self.path) - 1)
-        j = random.randint(0, len(self.path) - 1)
+    def inversion(self, i=None, j=None):
+        if i == None or j == None:
+            i = random.randint(0, len(self.path) - 1)
+            j = random.randint(0, len(self.path) - 1)
+        while i == j:
+            j = random.randint(0, len(self.path) - 1)
+            
         if i > j:
             i, j = j, i
         # Dist betwenn i & j
         sub_length = (j - i + 1)
         for k in range(sub_length // 2):
-            self.path[i + k], self.path[j -
-                                        k] = self.path[j - k], self.path[i + k]
+            self.path[i + k], self.path[j - k] = self.path[j - k], self.path[i + k]
         self.evaluate()
+        
+        print(f"Inverted cities from {i} to {j}\n")
 
     def insert(self):
-        x = 4
-        
+        # Placeholder
+        pass
     
+    def printPath(self, path_list):
+        for i, city in enumerate(path_list):
+            print(f"City {i}: ({city.point.x}, {city.point.y})")
 
-    def saveBest(self, mutation=None):
-        currentPath = self.path
+    def performMutation(self, mutation=None):
+        currentPath = self.path.copy()
         currentCost = self.evaluate()
 
         if mutation == "swap":
             self.swap()
         elif mutation == "inversion":
-            self.inversion
+            self.inversion()
         elif mutation == "insert":
             self.insert()
-            
+        else:
+            print("Invalid mutation operation.")
+
         if self.evaluate() > currentCost:
             self.path = currentPath
             self.cost = currentCost
--- main.py
+++ main.py
@@ -3,33 +3,10 @@ import individual
 from population import Population
 
 if __name__ == "__main__":
-  # travel = TSP("st70.tsp")
-  # tsp_instance = tsp.TSP("tsp_instances/eil101.tsp")
-  # individual = individual.Individual(tsp_instance)
-  # path = individual.path
-  # print(path[0].point.x, path[0].point.y)
-  # print(individual.evaluate())
-  # print(individual.swap())
-
-
-    travel = tsp.TSP("tsp_instances/eil101.tsp")
-    print(f"Loaded {len(travel.Cities)} cities.")
-
-    # Check random path generation
-    travel.path = travel.randomPath()
-    print("Random tour cost:", travel.pathCost())
-
-    # Test population creation with mutation
-    pop = Population(travel, size=10, mutation='inversion')
-
-    sum = 0
-    
-    for i, ind in enumerate(pop.individuals):
-        print(f"Individual {i} cost: {ind.cost}")
-        sum += ind.cost
-
-
-    mean_cost = sum / len(pop.individuals)
-    best = pop.getBest()
-    print("Best individual cost:", best.cost)
-    print("Mean cost:", mean_cost)
+    tsp_instance = tsp.TSP("tsp_instances/eil101.tsp")
+    individual = individual.Individual(tsp_instance)
+    path = individual.path
+    print(path[0].point.x, path[0].point.y)
+    print(individual.evaluate(), "\n")
+    individual.performMutation("inversion")
+    print(individual.evaluate(), "\n")
