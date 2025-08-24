dd14588f4d74dd5d3a66e53955be1b0bc637a1ab
BryceYoung <bryce.young18@icloud.com>
2025-08-24 14:06:56 +0930

    Added Typehinting Removed last argument in class Individual as it is never used

--- final/code/individual.py
+++ final/code/individual.py
@@ -4,30 +4,30 @@ import random
 
 
 class Individual:
-    def __init__(self, tsp_instance, path=None):
-        self.tsp = tsp_instance
-        self.path = self.tsp.randomPath()
-        self.cost = self.tsp.pathCost(self.path)
+    def __init__(self, tsp_instance: tsp.TSP):
+        self.tsp: tsp.TSP = tsp_instance
+        self.path: list[tsp.City] = self.tsp.randomPath()
+        self.cost: float = self.tsp.pathCost(self.path)
 
-    def evaluate(self):
+    def evaluate(self) -> float:
         self.cost = self.tsp.pathCost(self.path)
         return self.cost
 
-    def getPath(self):
+    def getPath(self) -> list[tsp.City]:
         return self.path
 
-    def swap(self, i=None, j=None):
+    def swap(self, i: int | None = None, j: int | None = None):
         if i == None or j == None:
             i = random.randint(0, len(self.path) - 1)
             j = random.randint(0, len(self.path) - 1)
         while i == j:
             j = random.randint(0, len(self.path) - 1)
 
-        temp = self.path[i]
+        temp: tsp.City = self.path[i]
         self.path[i] = self.path[j]
         self.path[j] = temp
 
-    def inversion(self, i=None, j=None):
+    def inversion(self, i: int | None = None, j: int | None = None):
         if i == None or j == None:
             i = random.randint(0, len(self.path) - 1)
             j = random.randint(0, len(self.path) - 1)
@@ -37,12 +37,14 @@ class Individual:
         if i > j:
             i, j = j, i
         # Dist betwenn i & j
-        sub_length = (j - i + 1)
+        sub_length: int = (j - i + 1)
         for k in range(sub_length // 2):
             self.path[i + k], self.path[j - k] = self.path[j - k], self.path[i + k]
 
         # print(f"Inverted cities from {i} to {j}\n")
 
+
+    # Randomly set i or j if not given - ensures that if this occurs i & j are different values
     def fixCityIndex(self, i: int | None, j: int | None) -> tuple[int,int]:
         if i is None and j is None:
             i = random.randint(0, len(self.path) - 1)
@@ -71,11 +73,12 @@ class Individual:
 
     def scramble(self, i: int | None = None, j: int | None = None):
         i,j = self.fixCityIndex(i,j)
-        section = []
+        section: list[tsp.City] = []
         for _ in range(j-i):
             section.append(self.path.pop(i))
+        
         while len(section) > 0:
-            rand = random.randint(0, len(section)-1)
+            rand: int = random.randint(0, len(section)-1)
             self.path.insert(i, section.pop(rand))
 
         # print(f"Cities scrambled between {i} and {j}")
@@ -84,8 +87,8 @@ class Individual:
         for i, city in enumerate(self.path):
             print(f"City {i} ({city.id}): ({city.point.x}, {city.point.y})")
 
-    def performMutation(self, mutation_probability=0.05, mutation_type="swap"):
-        random_number = random.random()
+    def performMutation(self, mutation_probability: float = 0.05, mutation_type: str = "swap") -> 'Individual':
+        random_number: float = random.random()
         if (random_number > mutation_probability):
             return self
 
