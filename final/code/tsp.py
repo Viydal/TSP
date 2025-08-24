bbbe50fe2cdefeabdaa79b35022e1302b0f3a3ac
BryceYoung <bryce.young18@icloud.com>
2025-08-24 13:56:55 +0930

    Added/Fixed Typehinting in tsp.py Removed dprint function Removed tsp.run function Added Comments

--- final/code/tsp.py
+++ final/code/tsp.py
@@ -1,11 +1,6 @@
 import math
 import random
 
-
-def dprint(val):
-    print(val)
-
-
 class Point:
     x: float
     y: float
@@ -14,7 +9,7 @@ class Point:
         self.x = x
         self.y = y
 
-    def distance(self, other):
+    def distance(self, other: 'Point') -> float:
         return math.sqrt(math.pow(self.x - other.x, 2) + math.pow(self.y - other.y, 2))
 
     def print(self):
@@ -23,6 +18,7 @@ class Point:
 
 class City:
     point: Point
+    id: int
 
     def __init__(self, point: Point, city_id: int):
         self.point = point
@@ -33,52 +29,53 @@ class City:
 
 
 class TSP:
-    Cities: ["City"]
-    path: ["City"]
+    Cities: list[City]
+    path: list[City]
 
     def __init__(self, filename: str):
         self.Cities = []
         self.path = []
         self.open(filename)
-        pass
 
+    # Open a .tsp file and readin all cities
     def open(self, filename):
         f = open(filename)
-        line = ""
+        line: str = ""
+
         while line[0:9] != "DIMENSION":
             line = f.readline()
-        ex, count = line.split(":")
+        _, count = line.split(":")
         count = int(count)
-        # print(count)
+
         while line[0:18] != "NODE_COORD_SECTION":
             line = f.readline()
-        # print(line.strip())
         line = f.readline().strip()
+
+        # Read in cities
         while line[0:3] != "EOF":
-            # dprint(line)
-            ex, x, y = line.split()
-            ex = int(ex)
+            city_id, x, y = line.split()
+            city_id = int(city_id)
             y = float(y)
             x = float(x)
-            self.Cities.append(City(Point(x, y), ex))
+            self.Cities.append(City(Point(x, y), city_id))
             line = f.readline().strip()
-        # print(f.readline().strip())
 
-    def pathCost(self, path=None):
+    # Calculate the cost of a path - defaults to the tsp instances path if not provided
+    def pathCost(self, path: list[City] | None = None) -> float:
         if path is None:
             path = self.path
-        cost = 0
+        cost: float = 0
         for i in range(len(path) - 1):
             cost += path[i].point.distance(path[i + 1].point)
+
+        # Add the path cost from the last city to the first, to complete the loop
         cost += path[-1].point.distance(path[0].point)
         return cost
 
-    def randomPath(self):
-        cities = self.Cities.copy()
-        path = []
+    # Create a random path from the tsp instances cities
+    def randomPath(self) -> list[City]:
+        cities: list[City] = self.Cities.copy()
+        path: list[City] = []
         for i in range(len(cities)):
             path.append(cities.pop(random.randint(0, len(cities) - 1)))
         return path
-
-    def run(self):
-        pass
